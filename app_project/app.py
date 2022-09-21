ed305ec3a7790e7dc4f043fe272c675f09a9d967
artnicanov <108960141+artnicanov@users.noreply.github.com>
2022-09-21 16:50:12 +0300

    Update app.py

--- app_project/app.py
+++ app_project/app.py
@@ -11,9 +11,9 @@ app = Flask(__name__)
 app.register_blueprint(main_blueprint)
 app.register_blueprint(loader_blueprint)
 
-@app.route("/uploads/<path:path>")
-def static_dir(path):
-    return send_from_directory("uploads", path)
+# @app.route("/uploads/<path:path>")
+# def static_dir(path):
+#     return send_from_directory("uploads", path)
 
 app.run()
 
