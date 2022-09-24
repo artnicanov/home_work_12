e039a53a2cf0499091bb2c52431ada6cfe21bdaa
artnicanov <108960141+artnicanov@users.noreply.github.com>
2022-09-24 14:29:33 +0300

    Update view.py

--- app_project/main/view.py
+++ app_project/main/view.py
@@ -1,21 +1,22 @@
 from flask import Blueprint, render_template, request
 from app_project.functions import get_posts_by_word
 
-# создаем блюпринт
+# создаем блюпринт, передаем ему имя и папку с шаблонами, чтобы потом можно было их брать для вьюшек
 main_blueprint = Blueprint('main_blueprint', __name__, template_folder='templates')
 
 # вьюшка для главной страницы
 @main_blueprint.route('/')
 def main_page():
-	return render_template('index.html')
+	return render_template('index.html') # просто возвращаем готовый шаблон
 
 # вьюшка для поиска
 @main_blueprint.route('/search/')
 def search_page():
 	search_query = request.args.get('s') # получаем значение из адресной строки через args
 	try:
-		posts = get_posts_by_word(search_query) # передаем функции, которая ищет слова значени полученное выше
+		posts = get_posts_by_word(search_query) # передаем функции, которая ищет слова, значение полученное строкой выше
 	except FileNotFoundError:
 		return "Файл не найден"
 
-	return render_template("post_list.html", query=search_query, posts=posts) # передаем в шаблон результаты поиска
+	# передаем в шаблон название и результаты поиска как переменные
+	return render_template("post_list.html", query=search_query, posts=posts)
